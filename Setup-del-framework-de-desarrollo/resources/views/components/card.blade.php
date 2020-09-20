7031425670fe4f485e438c4b4225a9943fd9e2fd
Alberto <A01207490@itesm.mx>
2020-09-19 22:34:04 -0500

    Instalación de bootstap y más colores

--- Setup-del-framework-de-desarrollo/resources/views/components/card.blade.php
+++ Setup-del-framework-de-desarrollo/resources/views/components/card.blade.php
@@ -1,11 +1,12 @@
 <div class="p-2 d-flex justify-content-center">
     <div class="card" style="width: 18rem;">
         <img src="{{ url('img/autores/' . $isbn . '.jpg') }}" class="card-img-top" alt="...">
-        <div class="card-body">
+        <div class="card-body bg-mist">
             <h5 class="card-title">{{ $autor["nombre"] }}</h5>
             <h6 class="card-subtitle mb-2 text-muted">{{ $autor["matricula"] }}</h6>
             <h6 class="card-subtitle mb-2">Experiencia</h6>
             <p class="card-text">{{ $autor["experiencia"] }}</p>
+            <button class="btn btn-red-darken-3">Details</button>
         </div>
     </div>
 </div>
--- Setup-del-framework-de-desarrollo/resources/views/layout.blade.php
+++ Setup-del-framework-de-desarrollo/resources/views/layout.blade.php
@@ -8,9 +8,8 @@
     <title>Laravel</title>
     @section("css")
     <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="/css/app.css">
     @show
 </head>
 
