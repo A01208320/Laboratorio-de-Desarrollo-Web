90e13e7ee74e98fb8a3eae634be4fba872646482
Alberto <A01207490@itesm.mx>
2020-10-07 12:19:13 -0500

    Creación de migraciones

---
+++ sparrow/database/migrations/2020_10_07_170708_create_reviews_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('title_id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('title_id')
+                ->references('id')
+                ->on('titles')
+                ->onDelete('cascade');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+            $table->unique(['title_id', 'user_id']);
+            $table->boolean('recomendation');
+            $table->text('comment');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
